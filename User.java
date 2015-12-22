8a3fbd07ae9e0caa95bb9db36c93c972945ada06
mcgml <mcgml@cardiff.ac.uk>
2015-12-22 14:48:51 +0000

    added user class

---
+++ User.java
@@ -0,0 +1,13 @@
+package nhs.genetics.cardiff;
+
+/**
+ * Created by ml on 22/12/2015.
+ */
+public class User {
+    public String FullName;
+    public String Password;
+    public String JobTitle;
+    public String UserId;
+    public String ContactNumber;
+    public Boolean Admin;
+}
